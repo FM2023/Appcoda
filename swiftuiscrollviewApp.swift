029e174195f032386aaef549e232dc29d71c6439
SEONG-HYUN LEE <122852215+FM2023@users.noreply.github.com>
2023-02-20 20:08:41 +0900

    Add files via upload

---
+++ swiftuiscrollviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftuiscrollviewApp.swift
+//  swiftuiscrollview
+//
+//  Created by 이은권 on 2023/02/18.
+//
+
+import SwiftUI
+
+@main
+struct swiftuiscrollviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
